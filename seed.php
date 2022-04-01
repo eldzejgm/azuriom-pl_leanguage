b1a68ea6cc5c976629dff2a1523d761b0afe0115
EldzejGM <78844172+eldzejgm@users.noreply.github.com>
2022-04-01 21:43:57 +0200

    Create seed.php

---
+++ seed.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Seed Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for seeding during installation.
+    |
+    */
+
+    'navbar' => [
+        'home' => 'Home',
+    ],
+
+    'roles' => [
+        'member' => 'Członek',
+        'admin' => 'Administrator',
+    ],
+
+    'settings' => [
+        'copyright' => 'Copyright © :year Azuriom - All Rights Reserved.',
+        'money' => 'punkty',
+    ],
+];
